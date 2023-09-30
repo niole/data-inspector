209b7353a822b11ef432fc745acbaa221f22db8a
niole <niolenelson@gmail.com>
2023-09-30 13:49:21 -0700

    Document import data

--- src/importdataservice.rs
+++ src/importdataservice.rs
@@ -1,5 +1,11 @@
 use log::info;
 
+/// downloads data over http from the specified uri
+/// no auth
+/// chunks, encodes, and stores in vector db
+///
+/// # Arguments
+/// * `uri` - the location of the data
 pub fn  import_data(uri: &String) {
     info!("{}", uri);
 }
